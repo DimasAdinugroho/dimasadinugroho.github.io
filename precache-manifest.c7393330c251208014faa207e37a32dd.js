dd432681658fd2bced4479fc4d8c0946b8d352a5
Dimas Adinugroho <dimasadinugroho92@gmail.com>
2020-09-25 14:19:45 +0700

    Updates

---
+++ precache-manifest.c7393330c251208014faa207e37a32dd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e78a9ae822de07ba1c45b8f3d582d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec027cc33e6b25ec45a5",
+    "url": "/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "17c3aab090db790e9961",
+    "url": "/static/css/main.d03e7ea4.chunk.css"
+  },
+  {
+    "revision": "ec027cc33e6b25ec45a5",
+    "url": "/static/js/2.31e02b6d.chunk.js"
+  },
+  {
+    "revision": "c728dbc5627530294e0e1007747e8a60",
+    "url": "/static/js/2.31e02b6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c3aab090db790e9961",
+    "url": "/static/js/main.38b0b679.chunk.js"
+  },
+  {
+    "revision": "3577d77348c9a55e21b2",
+    "url": "/static/js/runtime-main.cbd770fc.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "434b2129b3bef2d77b9b03bb130111fc",
+    "url": "/static/media/monumen-map.434b2129.jpeg"
+  },
+  {
+    "revision": "6e1fc5a3398d437a1c9ab48d2ff31f84",
+    "url": "/static/media/monumen.6e1fc5a3.jpg"
+  }
+]);
